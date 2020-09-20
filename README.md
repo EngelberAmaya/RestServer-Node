7369e9686eb7742703ae2f89e243559a7df43350
EngelberAmaya <engelberamaya@gmail.com>
2020-09-20 13:59:54 -0430

    RestServer - user

--- README.md
+++ README.md
@@ -1,12 +1,19 @@
 # Rest Server 
 
-Esta es una aplicación en Node.js, que permite hacer las peticiones de get, post, put y delete
+Esta es una aplicación en Node.js, que permite hacer las peticiones de Get de manera paginada, Post, Put con sus respectivas validaciones y Delete, tratando con la eliminación lógica de la entidad usuario
 
 
 Para ejecutarlo, es necesario reconstruir los módulos de node usando el siguiente comando
 
 ```
 npm install
+
+```
+
+Para correr la aplicación debe ejecutar el siguiente comando, en la carpeta del proyecto
+
+```
+node server/server ó nodemon server/server
 
 ```
 
---
+++ server/routes/usuario.js
@@ -0,0 +1,144 @@
+const express = require('express');
+const app = express();
+const bcrypt = require('bcrypt');
+const _ = require('underscore');
+const Usuario = require('../models/usuario');
+
+app.get('/usuario', function(req, res) {
+
+	let desde = req.query.desde || 0;
+	desde = Number(desde);
+
+	let limite = req.query.limite || 5;
+	limite = Number(limite);
+	
+	Usuario.find({estado: true}, 'nombre email role estado google img')
+			.skip(desde)
+		   	.limit(limite)
+			.exec( (err, usuarios) => {
+
+				if (err) {
+					return res.status(400).json({
+						ok: true,
+						err
+					})
+				}
+
+				Usuario.count({estado: true}, (err, conteo) => {
+
+					res.status(200).json({
+						ok: true,
+						usuarios,
+						total: conteo
+					});
+
+				})
+
+			})
+
+
+});
+
+
+app.post('/usuario', function(req, res) {
+
+	let body = req.body;
+
+	let usuario = new Usuario({
+		nombre: body.nombre,
+		email: body.email,
+		password: bcrypt.hashSync(body.password, 10),
+		role: body.role
+	});
+
+	usuario.save((err, usuarioDB) => {
+
+		if (err) {
+			return res.status(400).json({
+				ok: true,
+				err
+			})
+		}
+
+		//usuarioDB.password = null;
+
+		res.status(201).json({
+			ok: true,
+			usuario: usuarioDB
+		});
+
+	});
+	
+});
+
+app.put('/usuario/:id', function(req, res) {
+
+	let id = req.params.id;
+	let body = _.pick(req.body, ['nombre', 'email', 'img', 'role', 'estado']);
+
+	Usuario.findByIdAndUpdate(id, body, {new: true, runValidators: true}, (err,usuarioDB) => {
+
+		if (err) {
+			return res.status(400).json({
+				ok: true,
+				err
+			})
+		}
+
+		/*
+		if (!usuarioDB) {
+			return res.status(400).json({
+				ok:false,
+				mensaje: 'El usuario con el ' + id + 'no existe',
+				errors: { message: 'No existe un usuario con ese ID'}
+			});
+		}*/
+
+		res.status(200).json({
+			ok: true,
+			usuario: usuarioDB
+		});
+
+
+	})
+
+	
+});
+
+app.delete('/usuario/:id', function(req, res) {
+	
+	let id = req.params.id;
+	let cambiaEstado = {
+        estado: false
+    };
+
+    Usuario.findByIdAndUpdate(id, cambiaEstado, { new: true }, (err, usuarioBorrado) => {
+
+
+		if (err) {
+			return res.status(400).json({
+				ok: true,
+				err
+			});
+		}
+
+		if (!usuarioBorrado) {
+			return res.status(400).json({
+				ok: false,
+				err: {
+					message: 'Usuario no encontrado'
+				}
+			});
+		}
+
+		res.status(200).json({
+			ok: true,
+			usuario: usuarioBorrado
+		});
+
+	})
+
+});
+
+
+module.exports = app;
